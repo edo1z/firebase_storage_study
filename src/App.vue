2f3c824397d0472c0a208ef33cea739571e369db
edo1z <endo.yuta@logicky.com>
2018-08-23 02:16:35 +0900

    created menu bar and div box for dropping image file

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="menu">
+      <h1>firebase storage study</h1>
+    </div>
+    <div class="container">
+      <div class="left">
+        <div id="profile"></div>
+        <div id="dropbox">please drop your image files!</div>
+      </div>
+      <div class="middle">
+        <div id="imageList"></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name: 'app'
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html, body {
+  margin : 0;
+}
+#menu {
+  background : green;
+  color      : white;
+  padding    : 10px 5px;
+  margin     : 0;
+}
+#menu h1 {
+  font-size : 16px;
+  padding   : 0;
+  margin    : 0;
+}
+.container {
+  padding: 10px;
+}
+#dropbox {
+  padding: 10px;
+  width: 100px;
+  height: 100px;
+  background:orange;
 }
 </style>
